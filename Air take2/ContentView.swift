7f3e83fa451b9966e417ed0134ca08514a6b98dd
Ted Hardy <fomu65@gmail.com>
2019-12-26 11:53:07 -0500

    Initial Commit

---
+++ Air take2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Air take2
+//
+//  Created by Ted Hardy on 12/26/19.
+//  Copyright © 2019 High Gravity Consulting. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
